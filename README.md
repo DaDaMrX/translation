48bb5d7d34ea8ab039b60a5603eeeb5a83b1e71c
DaDaMrX <dadamrxx@gmail.com>
2019-09-28 13:54:42 +0800

    translate model

--- README.md
+++ README.md
@@ -12,20 +12,37 @@ Translation and Speech-to-text component of the final project in Information Ret
 
 ## Translation
 
+### Translate by Google Translate API
+
+- Make sure you can access Google first.
+
 Usage:
 ```shell
-$ python trainslate.py -t zh "hello"
+$ python trainslate.py -m api -t zh "hello"
 
 你好
 
-$ python trainslate.py -t en "你好"
+$ python trainslate.py -m api -t en "你好"
 
 Hello
 ```
 
-- [x] Translate with API  
-- [ ] Translate with model
+### Translate by NN model
 
+- lease download the pretrained model from [here](https://drive.google.com/uc?id=12UK2evibWAWwIkqKhvyfGsFD-ImrJ6Hn&export=download).
+
+- Unzip the file to the root directory.
+
+- Make sure you have installed [fairseq](https://github.com/pytorch/fairseq) and [PyTorch](https://pytorch.org).
+
+Usage:
+```shell
+$ python trainslate.py -m model -t zh "hello"
+
+$ python trainslate.py -m model -t en "你好"
+```
+
+Reference: [https://github.com/twairball/fairseq-zh-en](https://github.com/twairball/fairseq-zh-en)
 
 ## Speech-to-text
 
--- translate.py
+++ translate.py
@@ -4,13 +4,14 @@
 Authors:
     Heng-Da Xu <dadamrxx@gmail.com>
 """
-
 import argparse
+import re
+import subprocess
 
 import requests
 
 
-def translate(text, target='zh'):
+def translate_api(text, target):
     assert target in ['zh', 'en']
     if target == 'zh':
         source, target = 'en', 'zh'
@@ -29,14 +30,42 @@ def translate(text, target='zh'):
     return res
 
 
+def translate_model(text, target):
+    assert target in ['zh', 'en']
+    if target == 'zh':
+        source = 'en'
+        model_path = 'model/en2zh.pt'
+    else:
+        source = 'zh'
+        model_path = 'model/zh2en.pt'
+    cmd = (f'fairseq-interactive model '
+           f'--source-lang {source} --target-lang {target} '
+           f'--path {model_path} '
+           f'--beam 10 --remove-bpe --cpu')
+    output = subprocess.run(
+            cmd, input=text,
+            shell=True, check=True, 
+            capture_output=True, text=True
+    ).stdout
+    res = re.search(r'H-\d+\s+.+?\s+(.+)', output).group(1)
+    if target == 'zh':
+        res = re.sub(r'\s+', '', res)
+    return res
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Translate text between English and Chinese.')
-    parser.add_argument('--target', '-t', default='zh', choices=['zh', 'en'],
+    parser.add_argument('--method', '-m', choices=['api', 'model'], default='api',
+                        help='Which method to use (Google API or NN model)')
+    parser.add_argument('--target', '-t', choices=['zh', 'en'], default='zh',
                         help='Target language (Chinese or English)')
     parser.add_argument('text', 
                         help='Text to be translated')
     args = parser.parse_args()
 
-    res = translate(args.text, target=args.target)
+    if args.method == 'api':
+        res = translate_api(args.text, target=args.target)
+    else:
+        res = translate_model(args.text, target=args.target)
     print(res)
